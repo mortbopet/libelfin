b68ac7a3f84a8dd82b623521b4b5ab3fc03bf8dd
Morten Borup Petersen <morten_bp@live.dk>
2021-12-05 18:14:30 +0100

    Do not perform illegal memmove

--- dwarf/cursor.cc
+++ dwarf/cursor.cc
@@ -86,7 +86,8 @@ cursor::string(std::string &out)
         size_t size;
         const char *p = this->cstr(&size);
         out.resize(size);
-        memmove(&out.front(), p, size);
+        if(size != 0)
+            memmove(&out.front(), p, size);
 }
 
 const char *
